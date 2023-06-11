9802656c930425ccdeb670b924d03aeb7b9d97bb
rm1912 <riyamanna1912@gmail.com>
2023-06-11 14:50:46 +0530

    initial_commit

---
+++ Assets/Starter Package/CarBehaviour.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Collections;
+using UnityEngine;
+
+/**
+ * Our car will track a reticle and collide with a <see cref="PackageBehaviour"/>.
+ */
+public class CarBehaviour : MonoBehaviour
+{
+    public ReticleBehaviour Reticle;
+    public float Speed = 1.2f;
+
+    private void Update()
+    {
+        var trackingPosition = Reticle.transform.position;
+        if (Vector3.Distance(trackingPosition, transform.position) < 0.1)
+        {
+            return;
+        }
+
+        var lookRotation = Quaternion.LookRotation(trackingPosition - transform.position);
+        transform.rotation =
+            Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+        transform.position =
+            Vector3.MoveTowards(transform.position, trackingPosition, Speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var Package = other.GetComponent<PackageBehaviour>();
+        if (Package != null)
+        {
+            Destroy(other.gameObject);
+            
+        }
+    }
+}
